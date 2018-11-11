21bb95a077ff960baceb06426f70e8bae6a4efa4
Christian Panadero <panavtec@gmail.com>
2018-11-11 19:19:26 +0000

    use pointfree

--- backend/test/Stubs.hs
+++ backend/test/Stubs.hs
@@ -11,6 +11,7 @@ import           Control.Monad.Logger
 import           Control.Monad.State
 import           Control.Monad.Writer
 import           Data
+import           Data.Bifunctor            (first)
 import           Data.List                 (filter)
 import           IdGenerator
 import           Models
@@ -19,17 +20,23 @@ import           Servant                   (Handler, ServantErr)
 type GlobalState = ([User], [Post])
 newtype TestM a = TestM {
     runTestM :: WriterT [String] (StateT GlobalState Handler) a
-} deriving (Functor, Applicative, Monad, MonadIO, MonadState GlobalState, MonadError ServantErr)
+} deriving (
+  Functor,
+  Applicative,
+  Monad,
+  MonadIO,
+  MonadState GlobalState,
+  MonadError ServantErr)
 
 instance MonadDbRead User UserDbQueries TestM where
-  queryMany (UserByName name) = gets $ filter (\user -> userName user == name) . fst
-  queryMany (UserById _userId) = gets $ filter (\user -> userId user == _userId) . fst
+  queryMany (UserByName name)  = gets $ filter ((== name) . userName) . fst
+  queryMany (UserById _userId) = gets $ filter ((== _userId) . userId) . fst
 
 instance MonadDbWrite UserDbWrites TestM where
-  runCommand (InsertUser user) = modify $ \(users, posts) -> ((++ [user]) users, posts)
+  runCommand (InsertUser user) = modify $ first (++ [user])
 
 instance MonadDbRead Post PostDbQueries TestM where
-  queryMany (PostsByUserId _userId) = gets $ filter (\post -> postUserId post == _userId) . snd
+  queryMany (PostsByUserId _userId) = gets $ filter ((== _userId) . postUserId) . snd
 
 instance MonadDbWrite PostDbWrites TestM where
   runCommand _ = undefined
